e91c3386c0eb90be934612818e69b1e32e0838fb
Mason-Kelley9 <mtkelley@senators.ws.edu>
2023-07-24 12:31:02 -0400

    Added streamreader for 'movies to watch.txt' to read from movie list

--- MovieProgram/Movie Program/Movie Project.cs
+++ MovieProgram/Movie Program/Movie Project.cs
@@ -2,12 +2,32 @@ namespace Movie_Program
 {
     public partial class frmMovieProject : Form
     {
+        String line;
         public frmMovieProject()
         {
             InitializeComponent();
-        }
 
-        public List<string> movieList = new List<string>();
+            try
+            {
+                StreamReader sr = new StreamReader("E:\\movies to watch.txt");
+
+                line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    lstMovieList.Items.Add(line);
+
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
